4100b2e3f1483b16695499b921b76542be874e12
makexcake <v.nudelman@gmail.com>
2023-07-11 14:02:16 +0300

    added ecs cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,10 @@ variable vpc_cidr_block {}
 variable subnets_cidr {}
 variable avail_zones {}
 variable env_name {}
+
+
+variable "container_image" {}
+variable "container_name" {}
 # variable instance_type {}
 # variable linux_ami_id {}
 # variable windows_ami_id {}
@@ -21,17 +25,29 @@ module "vpc" {
 
   azs             = var.avail_zones
   private_subnets = var.subnets_cidr
+  public_subnets = []
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
-    Environment = "terraform"
+    Environment = var.env_name
   }
 }
 
+module "ecs-fargate" {
+  source  = "cn-terraform/ecs-fargate/aws"
+  version = "2.0.52"
 
+  # insert the 6 required variables here
+  container_image = var.container_image
+  container_name = var.container_name
+  name_prefix = var.env_name
+  private_subnets_ids = module.vpc.private_subnets
+  public_subnets_ids = module.vpc.public_subnets
+  vpc_id = module.vpc.vpc_id
+}
 
 
 
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,9 @@
 env_name = "commit-hw-tf"
+# Vars for VPC module
 vpc_cidr_block = "10.10.0.0/16"
 subnets_cidr = ["10.10.1.0/24", "10.10.2.0/24"]
 avail_zones = ["eu-central-1a", "eu-central-1b"]
+
+# Vars for ecs fargate module
+container_image = "528100219426.dkr.ecr.eu-central-1.amazonaws.com/homework-app-2:1.0"
+container_name = "hommework-container"
