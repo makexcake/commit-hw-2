6af22ca7a30bcf7e76bad8cd8e31c59042b2ce3a
makexcake <v.nudelman@gmail.com>
2023-07-11 14:25:58 +0300

    added tf backend config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  # bucket for storing tfstate 
+  backend "s3" {
+    bucket = "bucket-for-terraform-huipipa"
+    key = "myapp/terraform.tfstate"
+    region = "eu-central-1"
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
