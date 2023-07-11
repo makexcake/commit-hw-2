e9d069bdbbd542d4273b3ce987e3f46f45ee3dea
makexcake <v.nudelman@gmail.com>
2023-07-11 13:19:39 +0300

    appended values

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,35 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+
 variable vpc_cidr_block {}
-variable subnet_1_cidr_block {}
-variable subnet_2_cidr_block {}
-variable avail_zone_1 {}
-variable avail_zone_2 {}
+variable subnets_cidr {}
+variable avail_zones {}
 variable env_name {}
-variable instance_type {}
-variable linux_ami_id {}
-variable windows_ami_id {}
+# variable instance_type {}
+# variable linux_ami_id {}
+# variable windows_ami_id {}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "${var.env_name}-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             = var.avail_zones
+  private_subnets = var.subnets_cidr
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "terraform"
+  }
+}
+
 
 
 
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+env_name = "commit-hw-tf"
+vpc_cidr_block = "10.10.0.0/16"
+subnets_cidr = ["10.10.1.0/24", "10.10.2.0/24"]
+avail_zones = ["eu-central-1a", "eu-central-1b"]
