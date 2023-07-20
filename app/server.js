922090f3ba09fde2fb4c37df42ab6cdc04fb3773
makexcake <v.nudelman@gmail.com>
2023-07-20 20:41:29 +0300

    modified app

--- app/server.js
+++ app/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const AWS = require('aws-sdk');
 const path = require('path');
+const Stream = require('stream');
 
 const app = express();
 
-// Set up static file serving
-app.use(express.static(path.join(__dirname, '')));
+// Set up AWS SDK and the S3 client
+AWS.config.update({region:'eu-central-1'});
+const s3 = new AWS.S3();
+
+app.get('/image', function (req, res) {
+  const s3params = {
+    Bucket: 'commit-task-2',
+    Key: 'putin-monke.jpg'
+  };
+
+  s3.getObject(s3params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+      res.status(500).send("Error fetching image");
+    } else {
+      const img = new Stream.PassThrough();
+      img.end(data.Body);
+      res.set({
+        'Content-Type': 'image/jpeg', 
+        'Content-Length': data.ContentLength,
+        'Last-Modified': data.LastModified,
+        'ETag': data.ETag
+      });
+      img.pipe(res);
+    }
+  });
+});
 
 // Route handler for the homepage
 app.get('/', function (req, res) {
